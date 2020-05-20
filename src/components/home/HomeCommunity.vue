c061336896cc4ee71ff31579f0bf121882262460
arepson <arepp.son@gmail.com>
2020-05-20 21:02:49 +0700

    Add Link

--- src/components/home/HomeCommunity.vue
+++ src/components/home/HomeCommunity.vue
@@ -2,28 +2,28 @@
   <Section>
     <div class="grid-cols container container-md grid-cols--4">
     	<Card class="sponsor text-center">
-	      <g-image width="100" src="~/assets/images/mitra/icrc.svg" alt="ICRC"/>
+	    	<a href="//icrc.org/" rel="noopener" target="_blank"><g-image width="100" src="~/assets/images/mitra/icrc.svg" alt="ICRC"/></a>
     	</Card>
-		<Card class="sponsor text-center">
-			<g-image width="120" src="~/assets/images/mitra/ifrc.svg" alt="IFRC"/>
+			<Card class="sponsor text-center">
+		<a href="//ifrc.org/" rel="noopener" target="_blank"><g-image width="120" src="~/assets/images/mitra/ifrc.svg" alt="IFRC"/></a>
 		</Card>
 		<Card class="sponsor text-center">
-			<g-image width="120" src="~/assets/images/mitra/pmi.svg" alt="PMI"/>
+			<a href="//pmi.or.id/" rel="noopener" target="_blank"><g-image width="120" src="~/assets/images/mitra/pmi.svg" alt="PMI"/></a>
 		</Card>
 		<Card class="sponsor text-center">
-			<g-image width="120" src="~/assets/images/mitra/bmkg.svg" alt="BMKG"/>
+			<a href="//bmkg.go.id/" rel="noopener" target="_blank"><g-image width="120" src="~/assets/images/mitra/bmkg.svg" alt="BMKG"/></a>
 		</Card>
 		<Card class="sponsor text-center">
-			<g-image width="120" src="~/assets/images/mitra/bnpb.svg" alt="BNPB"/>
+			<a href="//bnpb.go.id/" rel="noopener" target="_blank"><g-image width="120" src="~/assets/images/mitra/bnpb.svg" alt="BNPB"/></a>
 		</Card>
 		<Card class="sponsor text-center">
 			<g-image width="120" src="~/assets/images/mitra/bpbd.svg" alt="BPBD"/>
 		</Card>
 		<Card class="sponsor text-center">
-			<g-image width="120" src="~/assets/images/mitra/jateng.svg" alt="Jateng"/>
+			<a href="//jatengprov.go.id/" rel="noopener" target="_blank"><g-image width="120" src="~/assets/images/mitra/jateng.svg" alt="Jateng"/></a>
 		</Card>
 		<Card class="sponsor text-center">
-			<g-image width="120" src="~/assets/images/mitra/kudus.svg" alt="Kudus"/>
+			<a href="//kuduskab.go.id/" rel="noopener" target="_blank"><g-image width="120" src="~/assets/images/mitra/kudus.svg" alt="Kudus"/></a>
 		</Card>
     </div>
   </Section>
